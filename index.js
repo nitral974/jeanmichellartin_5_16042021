82e8b9a637addc52d642539c6fc5de784cd8b01b
Jean-Michel LARTIN <nitral974@gmail.com>
2021-04-16 07:25:35 +0200

    premiere version

---
+++ index.js
@@ -0,0 +1,36 @@
+// Récupérer les éléments du DOM pour l'affichage des cartes produits
+
+const cardsProducts = document.querySelector(".cardsProducts");
+
+// Récupérer URL
+const searchUrl = window.location;
+console.log(searchUrl);
+
+// Récuperer les données de l'API en utilisant la méthode fetch
+
+fetch("http://localhost:3000/api/cameras")
+  .then((response) => response.json())
+  .then((data) => {
+    // Créer une boucle for afin d'afficher automatiquement la liste des produits
+    for (i = 0; i < data.length; i++) {
+      cardsProducts.innerHTML += ` 
+    <a class="col-4 mt-5" href="produit.html?id=${data[i]._id}"> 
+        <div class="card border-dark bg-dark">
+            <img
+            src="${data[i].imageUrl}"
+            class="card-img-top"
+            alt="..."
+            />
+            <div class="card-body text-white">
+                <h5 class="card-title">${data[i].name}</h5>
+                <p class="card-text">
+                ${data[i].description}
+                </p>
+                <p class="card-text text-end fw-bold fs-5">${(
+                  data[i].price / 100
+                ).toFixed(2)} €</p>
+            </div>
+        </div>
+    </a>`;
+    }
+  });
---
+++ produit.js
@@ -0,0 +1,30 @@
+// Récupérer les éléments du DOM
+
+const imageDuProduit = document.querySelector(".imageDuProduit");
+const titreDuProduit = document.querySelector(".titreDuProduit");
+const descriptionDuProduit = document.querySelector(".descriptionDuProduit");
+const prixDuProduit = document.querySelector(".prixDuProduit");
+const listeDesOptions = document.querySelector(".listeDesOptions");
+console.log(imageDuProduit);
+
+//récupérer l'ID de la page
+// Utiliser QueryString SearchParams
+const searchParams = new URLSearchParams(window.location.search);
+// Mettre la réponse dans une variable
+const searchId = searchParams.get("id");
+// Récupérer les données de l'api en fonction du produits (_id)
+
+fetch(`http://localhost:3000/api/cameras/${searchId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    imageDuProduit.src = `${data.imageUrl}`;
+    titreDuProduit.innerHTML = `${data.name}`;
+    descriptionDuProduit.innerHTML = `${data.description}`;
+    prixDuProduit.innerHTML = `${(data.price / 100).toFixed(2)} €`;
+    // Je crée une boucle for afin de gérer automatiquement la liste des options
+    for (i = 0; i < data.lenses.length; i++) {
+      nbOptions = document.createElement("option");
+      nbOptions.innerHTML = `${data.lenses[i]}`;
+      listeDesOptions.insertAdjacentElement("beforeend", nbOptions);
+    }
+  });
